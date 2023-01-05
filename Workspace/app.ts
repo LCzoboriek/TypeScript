fb7d2d581fe8be1e1c9a7ed7283ac44c3bb700c1
Luke-PC <lc169782@protonmail.com>
2023-01-05 12:04:37 +0000

    Added in boolean type check

--- Workspace/app.ts
+++ Workspace/app.ts
@@ -1,13 +1,18 @@
-function add(n1: number, n2: number) {
-  if (typeof n1 !== "number" || typeof n2 !== "number") {
-    throw new Error("Incorrect input!"); // This is how we throw an error in regular javascript, there is better ways to do this in typescript however
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //   throw new Error("Incorrect input!"); // This is how we throw an error in regular javascript, there is better ways to do this in typescript however
+  // }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
   }
-  return n1 + n2;
 }
 
+const resultPhrase = "Result is: ";
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
 
-const result = add(number1, number2);
-
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
