62bdd88d98943a956a87fd3d8e8650f8ba0bcd0f
Luke-PC <lc169782@protonmail.com>
2023-01-05 12:21:27 +0000

    Added in object check

--- Workspace/app.ts
+++ Workspace/app.ts
@@ -1,18 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //   throw new Error("Incorrect input!"); // This is how we throw an error in regular javascript, there is better ways to do this in typescript however
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
-}
+const person: {
+  name: string; //Key: Type Values for the object
+  age: number;
+} = {
+  name: "Max", //Key: Value pairs for the object
+  age: 30,
+};
 
-const resultPhrase = "Result is: ";
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
