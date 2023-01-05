552db920a56bae3c46e2c9b354804efd13e7885d
Luke-PC <lc169782@protonmail.com>
2023-01-05 12:26:25 +0000

    optimised the object defining

--- Workspace/app.ts
+++ Workspace/app.ts
@@ -1,8 +1,10 @@
-const person: {
-  name: string; //Key: Type Values for the object
-  age: number;
-} = {
-  name: "Max", //Key: Value pairs for the object
+// const person: {
+//   name: string; //Key: Type Values for the object
+//   age: number;
+// } = {
+const person = {
+  //This is the same as the above, but typescript can infer the type of the object from the values we assign to it
+  name: "Max", //Key: Value pairs for the object, this is more optimal then explicitly defining the type of the object
   age: 30,
 };
 
