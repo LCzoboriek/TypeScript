b4c4f0a6a65eb4acdf1d8bdbbaa32bd12d95976a
Luke-PC <lc169782@protonmail.com>
2023-01-09 12:42:12 +0000

    Started working on tuples

--- Workspace/app.ts
+++ Workspace/app.ts
@@ -6,6 +6,17 @@ const person = {
   //This is the same as the above, but typescript can infer the type of the object from the values we assign to it
   name: "Max", //Key: Value pairs for the object, this is more optimal then explicitly defining the type of the object
   age: 30,
+  hobbies: ["Sport", "Cooking"],
+  role: [2, "author"], // This is a tuple, a tuple is an array with a fixed length and fixed types
 };
 
+let favouriteActivities: string[]; // This is how we define an array of strings explicitly
+
+favouriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase()); // You can use the toUpperCase() method on strings as it is of type string array
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
